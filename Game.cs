9f4d6e5917477a1f5c4f6f01274f1c63d6b5a5e6
Max <max@maximilian.co.nz>
2023-06-02 23:32:31 +1200

    Created initial SFML project

---
+++ Game.cs
@@ -0,0 +1,32 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+class Game
+{
+	public void Run()
+	{
+		// Create the SFML window
+		RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Falling Obstacles");
+		window.SetFramerateLimit(60);
+		window.Closed += (sender, e) => window.Close();
+
+
+
+		while (window.IsOpen)
+		{
+			// Handle events
+			window.DispatchEvents();
+
+			
+
+			
+
+			// Clear the window
+			window.Clear(Color.Magenta);
+
+			// Display the contents of the window
+			window.Display();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Make, then run the game
+        Game game = new Game();
+        game.Run();
+    }
+}
